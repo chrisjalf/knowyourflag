45bd2825aa486cc00ca9f3893c7a45585f2cfff4
Chris James <xcastroj97@gmail.com>
2024-06-03 20:39:24 +0800

    UIImageView for flag

--- knowyourflag/Screens/GameVC.swift
+++ knowyourflag/Screens/GameVC.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class GameVC: UIViewController {
+    
+    let flagImageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        configureFlagImageView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -20,5 +23,24 @@ class GameVC: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: false)
         navigationController?.navigationBar.tintColor = .label
         tabBarController?.tabBar.isHidden = true
+    }
+    
+    private func configureFlagImageView() {
+        view.addSubview(flagImageView)
+        let selectedCountryIndex = Int.random(in: 0...Country.all.count - 1)
+        let selectedCountry = Country.all[selectedCountryIndex]
+        flagImageView.translatesAutoresizingMaskIntoConstraints = false
+        flagImageView.image = UIImage(named: selectedCountry.code)
+        flagImageView.layer.borderColor = UIColor.gray.cgColor
+        flagImageView.layer.borderWidth = 1.0
+        flagImageView.layer.cornerRadius = 10.0
+        flagImageView.clipsToBounds = true
+        
+        NSLayoutConstraint.activate([
+            flagImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            flagImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            flagImageView.widthAnchor.constraint(lessThanOrEqualTo: view.widthAnchor, multiplier: 0.8),
+            flagImageView.heightAnchor.constraint(equalTo: flagImageView.widthAnchor),
+        ])
     }
 }
