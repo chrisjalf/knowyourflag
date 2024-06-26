f5ab4f6562b1074c152d94ca3c998263e8a88a42
Chris James <xcastroj97@gmail.com>
2024-06-26 17:03:19 +0800

    remove all subviews from ProfileVC after tapping logout

--- knowyourflag/Screens/ProfileVC.swift
+++ knowyourflag/Screens/ProfileVC.swift
@@ -8,22 +8,62 @@
 import UIKit
 
 class ProfileVC: UIViewController {
+    private var isAuthenticated: Bool!
+    private var profile: ProfileResponse? = nil
+    private let logoutButton = KYFButton(backgroundColor: .systemPink, title: "Logout")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
         APIManager.sharedInstance.profile { [weak self] result in
             guard let _ = self else { return }
             
             switch result {
             case .success(let data):
-                print("profile: \(data)")
+                self?.profile = data
+                self?.isAuthenticated = true
+                DispatchQueue.main.async {
+                    self?.configureLogoutButton()
+                }
                 break
-            case .failure(let err):
-                print("error: \(err)")
+            case .failure(_):
+                self?.profile = nil
+                self?.isAuthenticated = false
                 break
             }
         }
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    private func configureLogoutButton() {
+        view.addSubview(logoutButton)
+        logoutButton.addTarget(self, action: #selector(logout), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            logoutButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            logoutButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            logoutButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func removeSubviews() {
+        for v in view.subviews {
+           v.removeFromSuperview()
+        }
+    }
+    
+    @objc private func logout() {
+        KeychainManager.sharedInstance.delete(service: "access_token", account: "kyf")
+        removeSubviews()
+    }
 }
