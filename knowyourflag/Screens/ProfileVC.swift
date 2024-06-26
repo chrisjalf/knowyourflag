442bdcf9ad912a2e09d7054f867e625463341772
Chris James <xcastroj97@gmail.com>
2024-06-26 22:22:34 +0800

    display email & name in profile vc

--- knowyourflag/Screens/ProfileVC.swift
+++ knowyourflag/Screens/ProfileVC.swift
@@ -13,6 +13,14 @@ class ProfileVC: UIViewController {
     private let logoutButton = KYFButton(backgroundColor: .systemPink, title: "Logout")
     private let loginButton = KYFButton(backgroundColor: .systemPink, title: "Login")
     
+    // profile fields
+    private let emailLabel = UILabel()
+    private let emailValueLabel = UILabel()
+    private let emailStackView = UIStackView()
+    private let nameLabel = UILabel()
+    private let nameValueLabel = UILabel()
+    private let nameStackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,6 +38,8 @@ class ProfileVC: UIViewController {
                 self?.profile = data
                 self?.isAuthenticated = true
                 DispatchQueue.main.async {
+                    self?.configureEmailStackView()
+                    self?.configureNameStackView()
                     self?.configureLogoutButton()
                 }
                 break
@@ -46,6 +56,55 @@ class ProfileVC: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+    }
+    
+    private func configureEmailStackView() {
+        view.addSubview(emailStackView)
+        
+        emailLabel.text = "Email"
+        emailLabel.textColor = .label
+        emailLabel.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        
+        emailValueLabel.text = profile?.email ?? "N/A"
+        emailValueLabel.textColor = .label
+        emailValueLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        
+        emailStackView.axis = .horizontal
+        emailStackView.distribution = .equalSpacing
+        emailStackView.addArrangedSubview(emailLabel)
+        emailStackView.addArrangedSubview(emailValueLabel)
+        emailStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            emailStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            emailStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            emailStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    private func configureNameStackView() {
+        view.addSubview(nameStackView)
+        
+        nameLabel.text = "Name"
+        nameLabel.textColor = .label
+        nameLabel.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        
+        nameValueLabel.text = profile?.name ?? "N/A"
+        nameValueLabel.textColor = .label
+        nameValueLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        
+        nameStackView.axis = .horizontal
+        nameStackView.distribution = .equalSpacing
+        nameStackView.addArrangedSubview(nameLabel)
+        nameStackView.addArrangedSubview(nameValueLabel)
+        nameStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nameStackView.topAnchor.constraint(equalTo: emailStackView.bottomAnchor, constant: 20),
+            nameStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            nameStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        
     }
     
     private func configureLogoutButton() {
